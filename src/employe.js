bd7c68b634e43994484f645c7b3030845bd33c46
L0rna <lorna.payetlsw@gmail.com>
2023-09-20 10:33:27 +0200

    second

--- src/employe.js
+++ src/employe.js
@@ -1,6 +1,16 @@
 "use strict";
 class Employe {
+    constructor(id, nom) {
+        this.id = id;
+        this.nom = nom;
+    }
     travailAvec() {
         console.log("");
+    }
+    assignerAnimal() {
+        console.log(`${this.nom} je t'assigne cet animal`);
+    }
+    retirerAnimal() {
+        console.log('');
     }
 }
